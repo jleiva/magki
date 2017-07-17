7d0e9b8ffac8105e6fcb48d9b196bfb7f50ba6a8
Jose Leiva <jdleiva@backcountry.com>
2017-07-16 22:35:27 -0600

    Ajustes en Pattern y agrego documentos

---
+++ prototipo/js/helpers/misc.js
@@ -0,0 +1,136 @@
+var misc = (function(window, undefined) {
+  // Espera un Array de Inputs
+  function buildDataObject(formInputs) {
+    var data = {};
+
+    formInputs.forEach(function(input) {
+      var fName = input.name;
+      var fValue = input.value;
+
+      data[fName] = fValue;
+    });
+
+    return data;
+  }
+
+  // https://davidwalsh.name/javascript-debounce-function
+  // Returns a function, that, as long as it continues to be invoked, will not
+  // be triggered. The function will be called after it stops being called for
+  // N milliseconds. If `immediate` is passed, trigger the function on the
+  // leading edge, instead of the trailing.
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+  // Espera un Array de Inputs
+  // Recorre cada Input y pone el atributo
+  // disabled en true
+  function disableFields(pFormInputs) {
+    pFormInputs.forEach(function(input) {
+      input.setAttribute('disabled', true);
+    });
+  }
+
+  // Deshabilita Inputs y boton de Guardar
+  function disableFieldsOnSave(pFormInputs) {
+    disableFields(pFormInputs);
+    document.querySelector('#btn-save').disabled = true;
+  }
+
+  // Deshabilita Inputs y boton de Guardar
+  function disableFieldsOnEdit(pFormInputs) {
+    disableFields(pFormInputs);
+    document.querySelector('#disable').checked = true;
+  }
+
+  // Espera un Array de Inputs
+  // Recorre cada Input y pone el atributo
+  // disabled en false
+  function enabledFields(pFormInputs) {
+    pFormInputs.forEach(function(input) {
+      input.disabled = false;
+    });
+
+    document.querySelector('#able').checked = true;
+  }
+
+  function enabledFieldsOnEdit(pFormInputs) {
+    enabledFields(pFormInputs);
+    document.querySelector('#able').checked = true;
+  }
+
+  // Lo idea es tener solo uno, pero tenemos problemas de datos
+  function populateOrgSelect(target, optList, array) {
+    if (!target){
+      return false;
+    }
+    else if(array) {
+      select = document.getElementById(target);
+
+      optList.forEach(function(item) {
+        var opt = document.createElement('option');
+        opt.value = item[1];
+        opt.innerHTML = opt.value;
+        select.appendChild(opt);
+      });
+    } else {
+      select = document.getElementById(target);
+
+      optList.forEach(function(item) {
+        var opt = document.createElement('option');
+        opt.value = item.organization;
+        opt.innerHTML = item.organization;
+        opt.setAttribute('id', item.codeNumber);
+        select.appendChild(opt);
+      });
+    }
+  }
+
+  // Populate SELECT
+  // Usage: populateSelect('selectElementId', arrayItems);
+  function populateSelect(target, optList, array) {
+    if (!target){
+      return false;
+    }
+    else if(array) {
+      select = document.getElementById(target);
+
+      optList.forEach(function(item) {
+        var opt = document.createElement('option');
+        opt.value = item[1];
+        opt.innerHTML = opt.value;
+        select.appendChild(opt);
+      });
+    } else {
+      select = document.getElementById(target);
+
+      optList.forEach(function(item) {
+        var opt = document.createElement('option');
+        opt.value = item.nombreLugar;
+        opt.innerHTML = opt.value;
+        select.appendChild(opt);
+      });
+    }
+  }
+
+  return {
+    buildDataObject: buildDataObject,
+    debounce: debounce,
+    disableFieldsOnEdit: disableFieldsOnEdit,
+    disableFieldsOnSave: disableFieldsOnSave,
+    enabledFieldsOnEdit: enabledFieldsOnEdit,
+    populateSelect: populateSelect,
+    populateOrgSelect: populateOrgSelect
+  };
+})(window);

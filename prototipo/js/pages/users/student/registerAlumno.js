a0dd7a64759d40bfc1c78a7131462ef957eec595
Jose Leiva <jdleiva@backcountry.com>
2017-08-20 02:00:32 -0600

    Preseleccionar Academia y profesor guia, cuando un Profesor registra a un alumno

--- prototipo/js/pages/users/student/registerAlumno.js
+++ prototipo/js/pages/users/student/registerAlumno.js
@@ -1,156 +1,191 @@
-var professorList = orm.findActiveProfs();
-var getValidBday = misc.debounce(function() {
-  validateDateBday();
-}, 1000);
+document.addEventListener('DOMContentLoaded', function() {
+  var logguedIn = orm.findLogguedUser();
+  var professorList = orm.findActiveProfs();
+  var academySelect = document.querySelector('#academy');
+  var getValidBday = misc.debounce(function() {
+    validateDateBday();
+  }, 1000);
 
-fillAcademies();
-setTournamentValidations();
+  fillAcademies();
+  setTournamentValidations();
 
-document.querySelector('#btn-save').addEventListener('click', registerStudent);
-document.querySelector('#bday').addEventListener('change', getValidBday);
-
-document.querySelector('#academy').addEventListener('change', function(e) {
-  fillProfessor(e);
-});
-
-function registerStudent(e) {
-  e.preventDefault();
-  var formInputs = document.querySelectorAll('#register-user .js-form-field:required');
-
-  if(!validate.emptyFields(formInputs)) {
-    var validForm = validate.fieldsValue('register-user');
-
-    if (!validForm[1].length) {
-      validateForm();
-    }
+  if (logguedIn.rol === '3') {
+    var activeProfData = orm.findActiveProfesorById(logguedIn.id);
+    academySelect.value = activeProfData.id_academia;
+    academySelect.disabled = true;
+    prePopulateProfessor();
+    document.querySelector('#professor').value = activeProfData.id_usuario;
+    document.querySelector('#professor').disabled = true;
   }
 
-  window.scrollTo(0, 0);
-}
+  document.querySelector('#btn-save').addEventListener('click', registerStudent);
+  document.querySelector('#bday').addEventListener('change', getValidBday);
 
-function setTournamentValidations() {
-  var inputs = document.querySelectorAll('.js-tourn-input');
+  academySelect.addEventListener('change', function(e) {
+    fillProfessor(e);
+  });
 
-  inputs.forEach(function(btn) {
-    var error = document.createElement('p');
-    error.innerHTML = 'Número debe ser mayor o igual cero'; 
-    error.classList.add('alert-failure');
-    error.hide();
-    btn.appendChild(error);
+  function registerStudent(e) {
+    e.preventDefault();
+    var formInputs = document.querySelectorAll('#register-user .js-form-field:required');
 
-    btn.addEventListener('keypress', function(e){
-      if(e.keyCode == 13){
-        if(btn.value < 0);
-        error.show;
+    if(!validate.emptyFields(formInputs)) {
+      var validForm = validate.fieldsValue('register-user');
+
+      if (!validForm[1].length) {
+        validateForm();
       }
+    }
+
+    window.scrollTo(0, 0);
+  }
+
+  function setTournamentValidations() {
+    var inputs = document.querySelectorAll('.js-tourn-input');
+
+    inputs.forEach(function(btn) {
+      var error = document.createElement('p');
+      error.innerHTML = 'Número debe ser mayor o igual cero'; 
+      error.classList.add('alert-failure');
+      error.hide();
+      btn.appendChild(error);
+
+      btn.addEventListener('keypress', function(e){
+        if(e.keyCode == 13){
+          if(btn.value < 0);
+          error.show;
+        }
+      })
     })
-  })
-}
-
-function getRegisterData() {
-  var formInputs = document.querySelectorAll('#register-user .js-form-field');
-  var studentsInfo = misc.buildDataObject(formInputs);
-  
-  orm.registrarAlumno(studentsInfo);
-  misc.disableFieldsOnSave(formInputs);
-}
-
-function validateForm() {
-  var idField = document.querySelector('#id');
-  var codeValue = idField.value;
-  var $alertBox = $util('.js-login-msg');
-  var studentInfo = orm.findStudentById(codeValue);
-
-  if (!studentInfo) {
-    getRegisterData();
-    idField.removeClass('error');
-
-    if ($alertBox) {
-      $alertBox.removeClass('alert-failure')
-        .addClass('alert-success')
-        .html(msg.key.saveSuccess);
-    } else {
-      $util('.js-form').insertAdjacentHTML('afterbegin',
-      '<span class="note alert alert-success js-login-msg">' + msg.key.saveSuccess+ '</span>');
-    }
-
-  } else {
-    idField.addClass('error');
-    if ($alertBox) {
-      $alertBox
-        .removeClass('alert-success')
-        .addClass('alert-failure')
-        .html('Ya hay un usuario registrado con la identificación indicada; no se realizó el registro');
-    } else {
-      $util('.js-form').insertAdjacentHTML('afterbegin',
-        '<span class="Ya hay un usuario registrado con la identificación indicada; no se realizó el registro</span>');
-    }
   }
 
-  window.scrollTo(0, 0);
-}
+  function getRegisterData() {
+    var formInputs = document.querySelectorAll('#register-user .js-form-field');
+    var studentsInfo = misc.buildDataObject(formInputs);
+    
+    orm.registrarAlumno(studentsInfo);
+    misc.disableFieldsOnSave(formInputs);
+  }
 
-function fillAcademies() {
- var academiesList = orm.findActiveAcad();
- var academiesField = document.querySelector('#academy');
+  function validateForm() {
+    var idField = document.querySelector('#id');
+    var codeValue = idField.value;
+    var $alertBox = $util('.js-login-msg');
+    var studentInfo = orm.findStudentById(codeValue);
 
- for(var i = 0; i < academiesList.length; i++) {
-    var options = document.createElement('option');
-    var academyName = academiesList[i]['nombre_academia'];
-    options.value = academiesList[i].id_academia;
-    options.text = academyName;
-    academiesField.add(options);
- }
-}
+    if (!studentInfo) {
+      getRegisterData();
+      idField.removeClass('error');
 
-function fillProfessor(e) {
-  var academySelect = $util('#academy');
-  var pacademyName = academySelect.options[academySelect.selectedIndex].innerText;
-  var professorField = document.querySelector('#professor');
-  var options = document.createElement('option');
+      if ($alertBox) {
+        $alertBox.removeClass('alert-failure')
+          .addClass('alert-success')
+          .html(msg.key.saveSuccess);
+      } else {
+        $util('.js-form').insertAdjacentHTML('afterbegin',
+        '<span class="note alert alert-success js-login-msg">' + msg.key.saveSuccess+ '</span>');
+      }
 
-  professorField.options.length = 0;
-  options.text = 'Seleccionar';
-  options.value = '';
-  professorField.add(options);
+    } else {
+      idField.addClass('error');
+      if ($alertBox) {
+        $alertBox
+          .removeClass('alert-success')
+          .addClass('alert-failure')
+          .html('Ya hay un usuario registrado con la identificación indicada; no se realizó el registro');
+      } else {
+        $util('.js-form').insertAdjacentHTML('afterbegin',
+          '<span class="Ya hay un usuario registrado con la identificación indicada; no se realizó el registro</span>');
+      }
+    }
 
-  for (var i = 0; i < professorList.length; i++) {
-    if (professorList[i].nombre_academia === pacademyName) {
+    window.scrollTo(0, 0);
+  }
+
+  function fillAcademies() {
+   var academiesList = orm.findActiveAcad();
+   var academiesField = document.querySelector('#academy');
+
+   for(var i = 0; i < academiesList.length; i++) {
       var options = document.createElement('option');
-      var professorName = professorList[i].primer_nombre + ' ' + professorList[i].primer_apeliido + ' ' + professorList[i].segundo_apellido;
-      options.value = professorList[i].id_usuario;
-      options.text = professorName;
-      professorField.add(options);
+      var academyName = academiesList[i]['nombre_academia'];
+      options.value = academiesList[i].id_academia;
+      options.text = academyName;
+      academiesField.add(options);
+   }
+  }
+
+  function fillProfessor(e) {
+    var academySelect = $util('#academy');
+    var pacademyName = academySelect.options[academySelect.selectedIndex].innerText;
+    var professorField = document.querySelector('#professor');
+    var options = document.createElement('option');
+
+    professorField.options.length = 0;
+    options.text = 'Seleccionar';
+    options.value = '';
+    professorField.add(options);
+
+    for (var i = 0; i < professorList.length; i++) {
+      if (professorList[i].nombre_academia === pacademyName) {
+        var options = document.createElement('option');
+        var professorName = professorList[i].primer_nombre + ' ' + professorList[i].primer_apeliido + ' ' + professorList[i].segundo_apellido;
+        options.value = professorList[i].id_usuario;
+        options.text = professorName;
+        professorField.add(options);
+      }
     }
   }
-}
 
-function validateDateBday() {
-  var bDate = document.querySelector('#bday');
-  var ageField = document.querySelector('#age');
-  var bDateValue = document.querySelector('#bday').value;
-  var today= new Date();
-  var bdayMsg = $util('.js-bday-error');
+  function prePopulateProfessor() {
+    var academySelect = $util('#academy');
+    var pacademyName = academySelect.options[academySelect.selectedIndex].innerText;
+    var professorField = document.querySelector('#professor');
+    var options = document.createElement('option');
 
-  if ((Date.parse(bDateValue) < Date.parse(today))) {
-    bdayMsg.addClass('is-hidden');
-    calculateAge(bDateValue);
-  } else {
-    bdayMsg.removeClass('is-hidden').addClass('alert-failure');
-    bDate.value = '';
-    ageField.value = '';
-  }
-}
+    professorField.options.length = 0;
+    options.text = 'Seleccionar';
+    options.value = '';
+    professorField.add(options);
 
-function calculateAge(bDateValue) {
-  var today = new Date();
-  var birthDate = new Date(bDateValue);
-  var age = today.getFullYear() - birthDate.getFullYear();
-  var months = today.getMonth() - birthDate.getMonth();
-  
-  if (months < 0 || (months === 0 && today.getDate() < birthDate.getDate())) {
-    age--;
+    for (var i = 0; i < professorList.length; i++) {
+      if (professorList[i].nombre_academia === pacademyName) {
+        var options = document.createElement('option');
+        var professorName = professorList[i].primer_nombre + ' ' + professorList[i].primer_apeliido + ' ' + professorList[i].segundo_apellido;
+        options.value = professorList[i].id_usuario;
+        options.text = professorName;
+        professorField.add(options);
+      }
+    }
   }
 
-  document.querySelector('#age').value = age;
-}
+  function validateDateBday() {
+    var bDate = document.querySelector('#bday');
+    var ageField = document.querySelector('#age');
+    var bDateValue = document.querySelector('#bday').value;
+    var today= new Date();
+    var bdayMsg = $util('.js-bday-error');
+
+    if ((Date.parse(bDateValue) < Date.parse(today))) {
+      bdayMsg.addClass('is-hidden');
+      calculateAge(bDateValue);
+    } else {
+      bdayMsg.removeClass('is-hidden').addClass('alert-failure');
+      bDate.value = '';
+      ageField.value = '';
+    }
+  }
+
+  function calculateAge(bDateValue) {
+    var today = new Date();
+    var birthDate = new Date(bDateValue);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var months = today.getMonth() - birthDate.getMonth();
+    
+    if (months < 0 || (months === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+
+    document.querySelector('#age').value = age;
+  }
+});
